a04ca5cffe2e4ef81287a95a10f115481065228a
Defaulthyu <default@g.shingu.ac.kr>
2025-04-15 00:26:50 +0900

    Fire함정 및 Fire애니메이션, 추적하는 적 구현

---
+++ Assets/Script/Fire_Trap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class Fire_Trap : MonoBehaviour
+{
+    public GameObject fireEffect;      // 바닥 위에 있는 불 오브젝트 (활성/비활성으로 ON/OFF)
+    public float fireOnTime = 1.5f;    // 불이 켜져있는 시간
+    public float fireOffTime = 2f;     // 불이 꺼져있는 시간
+
+    private void Start()
+    {
+        StartCoroutine(FireRoutine());
+    }
+
+    IEnumerator FireRoutine()
+    {
+        while (true)
+        {
+            // 불 켜기
+            fireEffect.SetActive(true);
+
+            yield return new WaitForSeconds(fireOnTime);
+
+            // 불 끄기
+            fireEffect.SetActive(false);
+
+            yield return new WaitForSeconds(fireOffTime);
+        }
+    }
+}
