94ba0731c28e6f6ae1e986b67294bafb47c28a05
Defaulthyu <default@g.shingu.ac.kr>
2025-04-19 15:42:31 +0900

    보스 모아이 애니메이션. 스테이지4 완료. 모아이, 지진 효과음

--- Assets/Script/Boss_Treasure.cs
+++ Assets/Script/Boss_Treasure.cs
@@ -11,12 +11,14 @@ public class BossEventManager : MonoBehaviour
     public Animator bossAnimator;
     public CinemachineImpulseSource impulseSource;
     public AudioSource bossSFX;
+    public AudioSource Crack;
+    public AudioSource hitsound;
 
     public float cameraShakeDelay = 1f;
     public float bossAwakeDelay = 2f;
     public float playerFlyForce = 500f;
     public float sceneChangeDelay = 2f;
-    public string nextSceneName = "Stage5";
+    public string nextSceneName = "Stage_5";
 
     private bool eventStarted = false;
 
@@ -36,7 +38,6 @@ public class BossEventManager : MonoBehaviour
         playerRb.velocity = Vector2.zero; // 플레이어 속도 초기화
 
         // 1. 플레이어 기뻐하는 애니메이션
-        Debug.Log("플레이어 보물 찾음");
         if (playerAnimator != null)
             playerAnimator.SetTrigger("FindTreasure");
 
@@ -45,27 +46,45 @@ public class BossEventManager : MonoBehaviour
 
         // 3. 카메라 흔들기
         if (impulseSource != null)
+        {
+            earthquake();
             impulseSource.GenerateImpulse();
+            Debug.Log("카메라 흔들림");
+        }
+
+        // 5. 보스 사운드 재생
+        if (bossSFX != null)
+            Debug.Log("보스 사운드 재생");
+            bossSFX.Play();
 
         // 4. 보스(석상) 깨어나는 애니메이션
         yield return new WaitForSeconds(bossAwakeDelay);
         if (bossAnimator != null)
             bossAnimator.SetTrigger("Awake");
+        // Invoke("earthquake", 7.57f);
 
 
-        // 5. 보스 사운드 재생
-        if (bossSFX != null)
-            bossSFX.Play();
 
-        // 6. 플레이어 날리기
-        yield return new WaitForSeconds(0.5f);
-        if (playerRb != null)
-        {
-            playerAnimator.SetTrigger("PlayerThrow");
-        }
+
 
         // 7. 5스테이지로 씬 전환
         yield return new WaitForSeconds(sceneChangeDelay);
         SceneManager.LoadScene(nextSceneName);
+    }
+    private void earthquake()
+    {
+        if (Crack != null)
+        {
+            Crack.Play();
+            Debug.Log("크랙 사운드 재생");
+        }
+    }
+    private void hit()
+    {
+        if(hitsound != null)
+        {
+            hitsound.Play();
+            Debug.Log("타격 사운드 재생");
+        }
     }
 }
